3a64cbe3955bd368da1707351251e8c3107328ee
Toby Wilds <34216527+TMWilds@users.noreply.github.com>
2020-02-29 20:38:38 +0000

    Integrate Vuetify (resolves #4) (#6)

--- ui/src/views/Dependents.vue
+++ ui/src/views/Dependents.vue
@@ -1,26 +1,38 @@
 <template>
-    <div id="nav" style="height: 50%; width: 100%">
-        <!--<router-link to="/">Home</router-link> |-->
-        <!--<router-link to="/about">About</router-link>-->
-        <input v-model="checked" type="checkbox">
-        <treemap v-show="checked" style="margin-bottom: 10rem"></treemap>
-        <sunburst v-show="!checked" :data="dummy">
-            <!-- Add behaviors -->
-            <template slot-scope="{ on, actions }">
-                <highlightOnHover v-bind="{ on, actions }"/>
-                <zoomOnClick v-bind="{ on, actions }"/>
-            </template>
+    <v-container>
+        <v-row>
+            <v-col md="6">
+                <div>
+                    <!--<router-link to="/">Home</router-link> |-->
+                    <!--<router-link to="/about">About</router-link>-->
+                    <input v-model="checked" type="checkbox">
+                    <treemap v-show="!checked" style="margin-bottom: 10rem"></treemap>
+                    <sunburst v-show="checked" :data="dummy">
+                        <!-- Add behaviors -->
+                        <template slot-scope="{ on, actions }">
+                            <highlightOnHover v-bind="{ on, actions }"/>
+                            <zoomOnClick v-bind="{ on, actions }"/>
+                        </template>
 
-            <!-- Add information to be displayed on top the graph -->
-            <nodeInfoDisplayer slot="top" slot-scope="{ nodes }" :current="nodes.mouseOver" :root="nodes.root"
-                               description="of visits begin with this sequence of pages"/>
+                        <!-- Add information to be displayed on top the graph -->
+                        <nodeInfoDisplayer slot="top" slot-scope="{ nodes }" :current="nodes.mouseOver"
+                                           :root="nodes.root"
+                                           description="of visits begin with this sequence of pages"/>
 
-            <!-- Add bottom legend -->
-            <breadcrumbTrail slot="legend" slot-scope="{ nodes, colorGetter, width }" :current="nodes.mouseOver"
-                             :root="nodes.root" :colorGetter="colorGetter" :from="nodes.clicked" :width="width"/>
+                        <!-- Add bottom legend -->
+                        <breadcrumbTrail slot="legend" slot-scope="{ nodes, colorGetter, width }"
+                                         :current="nodes.mouseOver"
+                                         :root="nodes.root" :colorGetter="colorGetter" :from="nodes.clicked"
+                                         :width="width"/>
 
-        </sunburst>
-    </div>
+                    </sunburst>
+                </div>
+            </v-col>
+            <v-col md="6">
+
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -43,7 +55,7 @@
             sunburst,
             zoomOnClick
         },
-              data: function () {
+        data: function () {
             return {
                 // dummy: {
                 //         "name": "org.jsoup",
@@ -103,9 +115,85 @@
                 //             }, {"name": "org.jsoup.Jsoup.connect", "size": 2}]
                 //         }]
                 //     }
-                dummy: {"name": "org.jsoup", "size": 2, "children": [{"name": "org.jsoup.select", "size": 1, "children": [{"name": "org.jsoup.select.Elements", "size": 1, "children": [{"name": "org.jsoup.select.Elements.first", "size": 1, "children": []}, {"name": "org.jsoup.select.Elements.select", "size": 1, "children": []}, {"name": "org.jsoup.select.Elements.attr", "size": 1, "children": []}]}]}, {"name": "org.jsoup.nodes", "size": 2, "children": [{"name": "org.jsoup.nodes.Node", "size": 1, "children": [{"name": "org.jsoup.nodes.Node.attr", "size": 1, "children": []}]}, {"name": "org.jsoup.nodes.Element", "size": 2, "children": [{"name": "org.jsoup.nodes.Element.select", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.tagName", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.children", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.text", "size": 2, "children": []}, {"name": "org.jsoup.nodes.Element.child", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.nextElementSibling", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.siblingElements", "size": 1, "children": []}, {"name": "org.jsoup.nodes.Element.getElementsByClass", "size": 1, "children": []}]}]}, {"name": "org.jsoup.Connection", "size": 2, "children": [{"name": "org.jsoup.Connection.userAgent", "size": 1, "children": []}, {"name": "org.jsoup.Connection.get", "size": 2, "children": []}]}, {"name": "org.jsoup.Jsoup", "size": 2, "children": [{"name": "org.jsoup.Jsoup.parse", "size": 1, "children": []}, {"name": "org.jsoup.Jsoup.connect", "size": 2, "children": []}]}]},
+                dummy: {
+                    "name": "org.jsoup",
+                    "size": 2,
+                    "children": [{
+                        "name": "org.jsoup.select",
+                        "size": 1,
+                        "children": [{
+                            "name": "org.jsoup.select.Elements",
+                            "size": 1,
+                            "children": [{
+                                "name": "org.jsoup.select.Elements.first",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.select.Elements.select",
+                                "size": 1,
+                                "children": []
+                            }, {"name": "org.jsoup.select.Elements.attr", "size": 1, "children": []}]
+                        }]
+                    }, {
+                        "name": "org.jsoup.nodes",
+                        "size": 2,
+                        "children": [{
+                            "name": "org.jsoup.nodes.Node",
+                            "size": 1,
+                            "children": [{"name": "org.jsoup.nodes.Node.attr", "size": 1, "children": []}]
+                        }, {
+                            "name": "org.jsoup.nodes.Element",
+                            "size": 2,
+                            "children": [{
+                                "name": "org.jsoup.nodes.Element.select",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.tagName",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.children",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.text",
+                                "size": 2,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.child",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.nextElementSibling",
+                                "size": 1,
+                                "children": []
+                            }, {
+                                "name": "org.jsoup.nodes.Element.siblingElements",
+                                "size": 1,
+                                "children": []
+                            }, {"name": "org.jsoup.nodes.Element.getElementsByClass", "size": 1, "children": []}]
+                        }]
+                    }, {
+                        "name": "org.jsoup.Connection",
+                        "size": 2,
+                        "children": [{
+                            "name": "org.jsoup.Connection.userAgent",
+                            "size": 1,
+                            "children": []
+                        }, {"name": "org.jsoup.Connection.get", "size": 2, "children": []}]
+                    }, {
+                        "name": "org.jsoup.Jsoup",
+                        "size": 2,
+                        "children": [{
+                            "name": "org.jsoup.Jsoup.parse",
+                            "size": 1,
+                            "children": []
+                        }, {"name": "org.jsoup.Jsoup.connect", "size": 2, "children": []}]
+                    }]
+                },
                 checked: false,
-                }
+            }
 
         }
     }
